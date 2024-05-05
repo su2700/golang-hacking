142cd22f8a60365c14eb8538e7432f5d77dce404
Haoling Liu <lhllfc@gmail.com>
2024-05-05 19:40:39 +0200

    update exec func

--- P1_exxecute_commands/main.go
+++ P1_exxecute_commands/main.go
@@ -7,15 +7,21 @@ import (
 	"os/exec"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-	Command := "ls"
-	cmd_obj := exec.Command(Command)
+func executeCommand(command string,args_arr []string ) (err error){
+	args := args_arr
+	cmd_obj := exec.Command(command,args...)
 	cmd_obj.Stdout = os.Stdout
 	cmd_obj.Stderr = os.Stderr
-	err := cmd_obj.Run()
+	err = cmd_obj.Run()
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
+		
 	}
+	return nil
+}
+
+func main() {
+	command := "ls"  
+	executeCommand(command, []string{"-l"})
 }
